4a36be9880a8b35eb595166a42c2f72fe6228a77
Taamannae Taabassum <taamannae@gmail.com>
2020-10-21 12:55:31 -0400

    Updates

---
+++ precache-manifest.c137d7af87f23d2e8ec40da96f786bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aadfcb6441b74c292fc600a5a1f29f7",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "7375eed0da04cec7d78a",
+    "url": "/website/static/css/main.5bf5adf5.chunk.css"
+  },
+  {
+    "revision": "eaa185b6971d8d2288f8",
+    "url": "/website/static/js/2.c7856c2a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.c7856c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7375eed0da04cec7d78a",
+    "url": "/website/static/js/main.dfbd6bcd.chunk.js"
+  },
+  {
+    "revision": "270aa2b15f6d2ed75b7a",
+    "url": "/website/static/js/runtime-main.5ab63fa2.js"
+  }
+]);
